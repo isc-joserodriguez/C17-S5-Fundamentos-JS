a8ae8007f4df81736856a9284f229217be52a6b6
José Rodriguez <isc.joserodriguez@gmail.com>
2023-03-27 19:57:38 -0700

    Tipos de datos

---
+++ main.js
@@ -0,0 +1,57 @@
+// Esto es un comentario
+
+/* 
+Comentario
+de
+varias
+líneas
+*/
+
+var nombre = "Andrea"; // Variable
+var nuevaVar;
+var apellido = "Sanchez";
+console.log(nombre + apellido); // Impresión en consola
+
+const planeta = "Tierra"; // Constante
+
+nuevaVar = "Valor";
+
+//String
+var cadena = "Andrea";
+cadena = "Andrea";
+cadena = 'Andrea "Andy" Sanchez';
+cadena = "Andrea 'Andy' Sanchez";
+cadena = 'Andrea "Andy" Sanchez';
+
+console.log(cadena);
+//Number
+var numero = 15;
+numero = Infinity;
+numero = NaN; // Not a number
+numero = 2.2e20; // notación cientifica [2.2 * (10^ 20)]
+numero = 0.012;
+numero = -0.012;
+console.log(numero);
+console.log(150);
+console.log("150");
+//Boolean
+var activo = true; // Sólo puede recibir true o false
+activo = false;
+console.log(activo);
+
+// Undefined
+var indefinido; //Se refiere a variables sin valor por definir
+console.log(indefinido);
+
+// Null
+var nulo = null; //Se refiere a una variable que no se le asigna valor
+console.log(nulo);
+
+/* 
+var dineroEnTarjeta = 1000;
+if(dineroEnTarjeta > 0){
+    sdfgdfg
+}else{
+    asdfasdfsf
+} 
+*/
