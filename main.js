0fdd39d050a6901dfe40358e346c6ad6df66dffb
José Rodriguez <isc.joserodriguez@gmail.com>
2023-03-29 18:34:09 -0700

    Funciones

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ líneas
 var nombre = "Andrea"; // Variable
 var nuevaVar;
 var apellido = "Sanchez";
-console.log(nombre + apellido); // Impresión en consola
+console.log(nombre + " " + apellido); // Impresión en consola
 
 const planeta = "Tierra"; // Constante
 
@@ -18,7 +18,7 @@ nuevaVar = "Valor";
 
 //String
 var cadena = "Andrea";
-cadena = "Andrea";
+cadena = 'And"rea';
 cadena = 'Andrea "Andy" Sanchez';
 cadena = "Andrea 'Andy' Sanchez";
 cadena = 'Andrea "Andy" Sanchez';
@@ -47,11 +47,29 @@ console.log(indefinido);
 var nulo = null; //Se refiere a una variable que no se le asigna valor
 console.log(nulo);
 
-/* 
-var dineroEnTarjeta = 1000;
-if(dineroEnTarjeta > 0){
-    sdfgdfg
-}else{
-    asdfasdfsf
-} 
-*/
+// Operador % (Modulo) Retorna el residuo de una división
+var modulo = 8 % 2; //Esto nos regresa un 0, porque el 8 es múltiplo de 2
+modulo = 7 % 2; //Esto nos regresa un 1, porque el 7 entre 2 nos deja con un 3 entero y sobra 1
+
+var dineroEnTarjeta = 0;
+if (dineroEnTarjeta > 0) {
+  console.log("Felicidades, tienes dinero!");
+} else {
+  console.log("Qué mal, no tienes dinero!");
+}
+
+function saluda(nombre, edad, ciudad) {
+  console.log(
+    "Hola " + nombre + " tu edad es de " +
+    edad + " y vives en: " + ciudad
+  );
+}
+
+saluda("Roberto", 24, "Ciudad Madero");
+saluda("María", 46, "Ciudad De México");
+
+var efectivo = 150;
+var producto = 100;
+
+
+
